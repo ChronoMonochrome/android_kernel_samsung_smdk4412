9abec4f3be98820d3840825a9ad239e49a815870
Scott James Remnant <scott@ubuntu.com>
2009-10-27 10:05:32 +0000

    trace: add trace events for open(), exec() and uselib()
        This patch uses TRACE_EVENT to add tracepoints for the open(),
        exec() and uselib() syscalls so that ureadahead can cheaply trace
        the boot sequence to determine what to read to speed up the next.
    
        It's not upstream because it will need to be rebased onto the syscall
        trace events whenever that gets merged, and is a stop-gap.
    
        Signed-off-by: Scott James Remnant <scott@ubuntu.com>

--- fs/open.c
+++ fs/open.c
@@ -31,6 +31,9 @@
 #include <linux/ima.h>
 #include <linux/dnotify.h>
 
+#define CREATE_TRACE_POINTS
+#include <trace/events/fs.h>
+
 #include "internal.h"
 
 int do_truncate(struct dentry *dentry, loff_t length, unsigned int time_attrs,
@@ -1035,6 +1038,7 @@ long do_sys_open(int dfd, const char __user *filename, int flags, umode_t mode)
 			} else {
 				fsnotify_open(f);
 				fd_install(fd, f);
+				trace_do_sys_open(tmp, flags, mode);
 			}
 		}
 		putname(tmp);
