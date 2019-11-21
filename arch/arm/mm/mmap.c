2231208cac0dd610bd4ef7c11be1642d8de4e6d8
Shilin Victor <chrono.monochrome@gmail.com>
2019-11-21 15:33:23 +0300

    arm: hack mmap for Midas devices
    Change-Id: I50fe8272119a84b4ba7de2ccc0637f9075fbeb48

--- arch/arm/mm/mmap.c
+++ arch/arm/mm/mmap.c
@@ -21,13 +21,7 @@
 
 static int mmap_is_legacy(void)
 {
-	if (current->personality & ADDR_COMPAT_LAYOUT)
-		return 1;
-
-	if (rlimit(RLIMIT_STACK) == RLIM_INFINITY)
-		return 1;
-
-	return sysctl_legacy_va_layout;
+	return 1;
 }
 
 static unsigned long mmap_base(unsigned long rnd)
