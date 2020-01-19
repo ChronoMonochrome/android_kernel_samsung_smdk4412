8da7f3c824216d50891b8a7119aacddaae87c394
Shilin Victor <chrono.monochrome@gmail.com>
2020-01-20 02:00:19 +0300

    Revert "ASoC: wm8994: Allow a delay between jack insertion and microphone detect"
    This reverts commit c7af821b90ee5af2afde0e2aaab9977f97a190e8.
    
    Change-Id: Ic038d62db9d3ed6551b4e9859e15dcb13154e2bb

--- include/linux/mfd/wm8994/pdata.h
+++ include/linux/mfd/wm8994/pdata.h
@@ -177,11 +177,6 @@ struct wm8994_pdata {
         unsigned int lineout1fb:1;
         unsigned int lineout2fb:1;
 
-	/* Delay between detecting a jack and starting microphone
-	 * detect (specified in ms)
-	 */
-	int micdet_delay;
-
 	/* IRQ for microphone detection if brought out directly as a
 	 * signal.
 	 */
