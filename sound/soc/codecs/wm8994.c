benji-cannot-code/fast-vc-linux-benchmark-run-3

ALSA: Fix typo in drivers sound
Correct spelling typo in debug messages within drivers/sound

Signed-off-by: Masanari Iida <standby24x7@gmail.com>
Signed-off-by: Takashi Iwai <tiwai@suse.de>

sound/soc/codecs/wm8994.c
@@ -3723,7 +3723,7 @@ static irqreturn_t wm8958_mic_irq(int irq, void *data)
 	} while (count--);
 
 	if (count == 0)
-		dev_warn(codec->dev, "No impedence range reported for jack\n");
+		dev_warn(codec->dev, "No impedance range reported for jack\n");
 
 #ifndef CONFIG_SND_SOC_WM8994_MODULE
 	trace_snd_soc_jack_irq(dev_name(codec->dev));

ChronoMonochrome/android_kernel_samsung_smdk4412

Change-Id: I0238891bd49d246a087402b302f48245d5d7774d

sound/soc/codecs/wm8994.c
@@ -3873,7 +3873,7 @@ static irqreturn_t wm8958_mic_irq(int irq, void *data)
 	} while (count--);
 
 	if (count == 0)
-		dev_err(codec->dev, "No impedence range reported for jack\n");
+		dev_err(codec->dev, "No impedance range reported for jack\n");
 
         pr_err("midas_wm1811: %s: WM8958_MIC_DETECT_3 reg=%d\n", __func__, reg);
 