vova7878/gnu_linux_kernel_samsung_i9300

modem_if: fix build errors

drivers/misc/modem_if/modem_prj.h
@@ -483,7 +483,7 @@ struct io_device {
 	struct modem_ctl *mc;
 	struct modem_shared *msd;
 
-	struct wake_lock wakelock;
+	struct wakeup_source wakelock;
 	long waketime;
 
 	/* DO NOT use __current_link directly

ChronoMonochrome/android_kernel_samsung_smdk4412

drivers/misc/modem_if/modem_prj.h
@@ -764,7 +764,7 @@ struct modem_ctl {
 	struct io_device *bootd;
 
 	/* Wakelock for modem_ctl */
-	struct wake_lock mc_wake_lock;
+	struct wakeup_source mc_wake_lock;
 
 	void (*gpio_revers_bias_clear)(void);
 	void (*gpio_revers_bias_restore)(void);