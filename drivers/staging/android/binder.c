0f2688da8baad08b425c162457d77d6b8de838b7
Shilin Victor <chrono.monochrome@gmail.com>
2022-03-17 15:10:17 +0300

    HACK: binder: remove security_binder_transaction() calls
    Change-Id: Idfdf045008832a3eee3543534829c1d6026d5a18

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -1662,9 +1662,6 @@ static int binder_translate_binder(struct flat_binder_object *fp,
 				  (u64)node->cookie);
 		return -EINVAL;
 	}
-	if (security_binder_transfer_binder(proc->tsk, target_proc->tsk))
-		return -EPERM;
-
 	ref = binder_get_ref_for_node(target_proc, node);
 	if (!ref)
 		return -EINVAL;
@@ -1702,8 +1699,6 @@ static int binder_translate_handle(struct flat_binder_object *fp,
 				  proc->pid, thread->pid, fp->handle);
 		return -EINVAL;
 	}
-	if (security_binder_transfer_binder(proc->tsk, target_proc->tsk))
-		return -EPERM;
 
 	if (ref->node->proc == target_proc) {
 		if (fp->hdr.type == BINDER_TYPE_HANDLE)
@@ -1772,12 +1767,6 @@ static int binder_translate_fd(int fd,
 		ret = -EBADF;
 		goto err_fget;
 	}
-	ret = security_binder_transfer_file(proc->tsk, target_proc->tsk, file);
-	if (ret < 0) {
-		ret = -EPERM;
-		goto err_security;
-	}
-
 	target_fd = task_get_unused_fd_flags(target_proc, O_CLOEXEC);
 	if (target_fd < 0) {
 		ret = -ENOMEM;
@@ -1995,10 +1984,6 @@ static void binder_transaction(struct binder_proc *proc,
 		if (target_proc == NULL) {
 			return_error = BR_DEAD_REPLY;
 			goto err_dead_binder;
-		}
-		if (security_binder_transaction(proc->tsk, target_proc->tsk) < 0) {
-			return_error = BR_FAILED_REPLY;
-			goto err_invalid_target_handle;
 		}
 		if (!(tr->flags & TF_ONE_WAY) && thread->transaction_stack) {
 			struct binder_transaction *tmp;
@@ -3290,10 +3275,8 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			ret = -EBUSY;
 			goto err;
 		}
-		ret = security_binder_set_context_mgr(proc->tsk);
-		if (ret < 0)
-			goto err;
 		if (context->binder_context_mgr_uid != -1) {
+
 			if (context->binder_context_mgr_uid != current->cred->euid) {
 				pr_err("binder: BINDER_SET_CONTEXT_MGR bad uid %d != %d\n",
 				       current->cred->euid,
