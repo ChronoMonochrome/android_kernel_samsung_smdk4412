98f38cd599b1b0fe0ae0bc0c1a1616b3ad46ce85
Shilin Victor <chrono.monochrome@gmail.com>
2022-09-18 07:35:31 +0300

    thermal: move to device
    Change-Id: I26755ba1c18da34501c08be8f8cf749f9b0216b3

--- scripts/ld-wrapper.py
+++ scripts/ld-wrapper.py
@@ -132,6 +132,7 @@ additional_build_rules = {
             "device/i9300/drivers/leds/built-in.o",
             "device/i9300/drivers/samsung/built-in.o",
             "device/i9300/drivers/sensor/built-in.o",
+            "device/i9300/drivers/thermal/built-in.o",
         ],
     }],
 }
