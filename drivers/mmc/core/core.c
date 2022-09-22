848815cb7980a92fce5787dbb63db59747e50999
Shilin Victor <chrono.monochrome@gmail.com>
2022-09-22 21:58:30 +0300

    Revert "mmc/core: add samsung changes"
    This reverts commit 4bfea158953ce480cdeae99129ee72e43b79140d.
    
    Change-Id: I050c046dcd0dd55d38a722b5cfb08dc9a8936889

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1034,7 +1034,7 @@ static void mmc_power_up(struct mmc_host *host)
 	mmc_delay(10);
 }
 
-void mmc_power_off(struct mmc_host *host)
+static void mmc_power_off(struct mmc_host *host)
 {
 	host->ios.clock = 0;
 	host->ios.vdd = 0;
@@ -1120,7 +1120,8 @@ void mmc_attach_bus(struct mmc_host *host, const struct mmc_bus_ops *ops)
 }
 
 /*
- * Remove the current bus handler from a host.
+ * Remove the current bus handler from a host. Assumes that there are
+ * no interesting cards left, so the bus is powered down.
  */
 void mmc_detach_bus(struct mmc_host *host)
 {
@@ -1136,6 +1137,8 @@ void mmc_detach_bus(struct mmc_host *host)
 	host->bus_dead = 1;
 
 	spin_unlock_irqrestore(&host->lock, flags);
+
+	mmc_power_off(host);
 
 	mmc_bus_put(host);
 }
@@ -1645,7 +1648,6 @@ void mmc_stop_host(struct mmc_host *host)
 
 		mmc_claim_host(host);
 		mmc_detach_bus(host);
-		mmc_power_off(host);
 		mmc_release_host(host);
 		mmc_bus_put(host);
 		return;
@@ -1854,9 +1856,6 @@ int mmc_pm_notify(struct notifier_block *notify_block,
 			host->bus_ops->remove(host);
 
 		mmc_detach_bus(host);
-		/* for BCM WIFI */
-		if (!(host->pm_flags & MMC_PM_IGNORE_SUSPEND_RESUME))
-			mmc_power_off(host);
 		mmc_release_host(host);
 		host->pm_flags = 0;
 		break;
