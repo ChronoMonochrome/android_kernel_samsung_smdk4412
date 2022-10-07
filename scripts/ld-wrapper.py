2fe7316d02a0586a83a0cacfad4773a3cc5b0335
Shilin Victor <chrono.monochrome@gmail.com>
2022-10-07 18:07:54 +0300

    HACK: arm: mm: add legacy version of iotable_init
    Change-Id: I30d496d95e8b549aaae6c96b983605d46c62683d

--- scripts/ld-wrapper.py
+++ scripts/ld-wrapper.py
@@ -53,6 +53,13 @@ additional_build_rules = {
            "arch/arm/kernel/suspend.o",
         ]
     }],
+    "arch/arm/mm/built-in.o" :
+    [{
+        "injected_objects":
+        [
+            "device/i9300/arch/arm/mm/mmu-legacy.o",
+        ],
+    }],
     "kernel/built-in.o" :
     [{
         "injected_objects":
