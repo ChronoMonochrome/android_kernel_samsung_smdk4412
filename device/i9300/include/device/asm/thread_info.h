57eea0bb62f883a732513026eebdddf0993b9715
Shilin Victor <chrono.monochrome@gmail.com>
2023-03-29 13:30:12 +0300

    i9300: add thread_info.h device header

---
+++ device/i9300/include/device/asm/thread_info.h
@@ -0,0 +1,17 @@
+/*
+ *  arch/arm/include/asm/thread_info.h
+ *
+ *  Copyright (C) 2002 Russell King.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#ifndef _THREAD_INFO_H
+#define _THREAD_INFO_H
+
+#include <asm/thread_info.h>
+
+#define TIF_FREEZE		19
+
+#endif
