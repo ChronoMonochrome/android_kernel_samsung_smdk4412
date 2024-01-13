71c5a109e8ff4ccffe27ad4f5f0241a54bea56ef
Shilin Victor <chrono.monochrome@gmail.com>
2024-01-13 23:08:42 +0300

    Revert "i9300: rtc: remove Android alarm driver"
    This reverts commit 803c6370d10c3c34ed74a42d098a5a0b06b48995.

--- scripts/ld-wrapper.py
+++ scripts/ld-wrapper.py
@@ -259,6 +259,8 @@ additional_build_rules = {
     [{
         "injected_objects":
         [
+            "device/i9300/drivers/rtc/alarm.o",
+            "device/i9300/drivers/rtc/alarm-dev.o",
             "device/i9300/drivers/rtc/rtc-max77686.o",
             "device/i9300/drivers/rtc/rtc-s3c.o",
         ],
